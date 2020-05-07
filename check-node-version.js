c7f70e1050500212c64f014d528a9fe7d75a16ae
Rody Kirwan <Rody.Kirwan@smartbear.com>
2020-05-07 16:24:45 +0100

    STEAM-3460 setup eslint and node version check

---
+++ check-node-version.js
@@ -0,0 +1,8 @@
+const semver = require('semver')
+const { engines } = require('./package.json')
+
+const version = engines.node
+
+if (!semver.satisfies(process.version, version)) {
+  throw new Error(`The current node version${process.version} does not satisfy the required version ${version} .`)
+}
