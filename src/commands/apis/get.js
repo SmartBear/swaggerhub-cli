9608f1a3e7515f5164399df3559e10e60768dd7d
michaelmelody91 <michael.melody@smartbear.com>
2020-05-11 15:03:58 +0100

    eslint changes

--- src/commands/apis/get.js
+++ src/commands/apis/get.js
@@ -1,31 +1,31 @@
-const {Command, flags} = require('@oclif/command')
-const fetch = require('node-fetch');
+const { Command, flags } = require('@oclif/command')
+const fetch = require('node-fetch')
 
 class GetAPICommand extends Command {
 
   static args = [
-    {name: 'identifier'},
+    { name: 'identifier' },
   ]
 
   static flags = {
     // can pass either --force or -f
-    json: flags.boolean({char: 'j'})
+    json: flags.boolean({ char: 'j' })
   }
 
   async run() {
-    const {args, flags} = this.parse(GetAPICommand)
+    const { args, flags } = this.parse(GetAPICommand)
     const identifier = args.identifier
-    let headers = {'Accept': 'application/yaml'}
-    if(flags.json) headers={'Accept': 'application/json'}
+    const headers = { 'Accept': 'application/yaml' }
+    if (flags.json) headers.Accept= 'application/json'
 
     fetch(`https://dev-api.swaggerhub.com/apis/${identifier}`, {
       headers: headers
     })
       .then(res => res.text())
-      .then(text => console.log(text));
+      .then(text => console.log(text))
   }
 }
 
-GetAPICommand.description = `Fetches an API version`
+GetAPICommand.description = 'Fetches an API version'
 
 module.exports = GetAPICommand
