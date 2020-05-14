77a4be3e49815aea82e38448c3b8605bb8535608
Rody Kirwan <Rody.Kirwan@smartbear.com>
2020-05-14 16:02:18 +0100

    Merge branch 'refactor/functional-approach' of https://github.com/SmartBear/swaggerhub-cmd into refactor/functional-approach

---
+++ test/utils/compositions.test.js
@@ -0,0 +1,21 @@
+const { expect, test } = require('@oclif/test')
+const { pipe } = require('../../src/utils/compositions')
+const mock = require('../__mocks__/config')
+const isEqual = require('lodash/isEqual')
+
+const { setConfig, getConfig } = require('../../src/services/config')
+
+describe('compositions ', () => {
+  describe('pipe', () => {
+    test.it('should pipe (left to right) ouput of 1st function through input of the next', () => {
+      const doubleIt = x => x * 2
+      const trebleIt = x => x * 3
+      const quadrupleIt = x => x * 4
+
+      const output = pipe(1)(doubleIt, trebleIt, quadrupleIt)
+      
+      expect(output).to.equal(24)
+    })
+  })
+})
+  
