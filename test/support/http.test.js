8c197eaf5ead30437c4f903c528e4dcfc7e405be
Rody Kirwan <Rody.Kirwan@smartbear.com>
2020-05-21 14:43:28 +0100

    Fix http tests for checking headers and reqBody

--- test/support/http.test.js
+++ test/support/http.test.js
@@ -1,19 +1,10 @@
 const { expect, test } = require('@oclif/test')
-const pick = require('lodash/pick')
 const http = require('../../src/support/http')
-const isEqual = require('lodash/isEqual')
-
-// For some reason nock wraps req.header values in an array.
-// We need to flatten these back to string values. This is pretty crap for testing purposes,
-// but there doesn't seem to be another way to assert the request headers.
-const formatNockHeaders = reqHeaders => Object.keys(reqHeaders)
-  .reduce((formattedHeaders, key) => ({
-    [key]: Array.isArray(reqHeaders[key]) ? reqHeaders[key].join() : reqHeaders[key],
-    ...formattedHeaders
-  }), {})
 
 const mockUrl = 'https://test.http.com'
 const mockApiKey = 'mock-api-key-1234'
+const mockReqBody = { test: '123', id: 'test' }
+const mockReqQuery = mockReqBody
 
 describe('http', () => {
   describe('default function', () => {
@@ -34,11 +25,10 @@ describe('http', () => {
     test
       .nock(mockUrl, api => api
         .get('/headers')
-        .reply(200, function(){
-          return {
-            headers: formatNockHeaders(this.req.headers)
-          }
-        })
+        .matchHeader('authorization', `Bearer ${mockApiKey}`)
+        .matchHeader('content-type', 'application/json')
+        .matchHeader('accept', 'application/json')
+        .reply(200)
       )
       .it('should create valid http headers from the provided options', async () => {
         const expectedHeaders = {
@@ -47,32 +37,38 @@ describe('http', () => {
           'authorization': `Bearer ${mockApiKey}`
         }
 
-        const request = await http({
+        return await http({
           url: [mockUrl, 'headers'],
           accept: 'json',
           contentType: 'json',
           auth: mockApiKey
         })
-
-        const { headers } = await request.json()
-
-        const requestHeaders = pick(headers, ['accept', 'content-type', 'authorization'])
-
-        expect(isEqual(requestHeaders, expectedHeaders)).to.equal(true)
     })
     test
       .nock(mockUrl, api => api
-        .get('/query?test=true&queryString=test')
+        .get('/query')
+        .query(mockReqQuery)
         .reply(200)
       )
       .it('should concatenate the provided query object to the url string', async () => {
-        const expectedUrl = `${mockUrl}/query?test=true&queryString=test`
+        const expectedUrl = `${mockUrl}/query?test=123&id=test`
         const { url } = await http({
           url: [mockUrl, 'query'],
-          query: { test: true, queryString: 'test' }
+          query: mockReqQuery
         })
 
         expect(url).to.equal(expectedUrl)
     })
+    test
+      .nock(mockUrl, api => api
+        .post('/body', mockReqBody)
+        .reply(200)
+      )
+      .it('should post the provided body option', async () => await http({
+          url: [mockUrl, 'body'],
+          method: 'post',
+          body: JSON.stringify(mockReqBody)
+        })
+      )
   })
 })
