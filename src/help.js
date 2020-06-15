9b97325f0afdf1354311297565d6a2d61833ff7b
conor-ward <conor.ward@smartbear.com>
2020-06-15 16:58:29 +0100

    Remove showTopicHelp function

--- src/help.js
+++ src/help.js
@@ -2,25 +2,6 @@ const Help = require('@oclif/plugin-help').default
 
 module.exports = class CustomHelp extends Help {
 
-  showTopicHelp(topic) {
-    const name = topic.name
-
-    const subTopics = this.sortedTopics.filter(t => t.name.startsWith(name.concat(':')))
-    const commands = this.sortedCommands.filter(c => c.id.startsWith(name.concat(':')))
-
-    console.log(this.formatTopic(topic))
-
-    if (subTopics.length > 0) {
-      console.log(this.formatTopics(subTopics))
-      console.log('')
-    }
-
-    if (commands.length > 0) {
-      console.log(this.formatCommands(commands))
-      console.log('')
-    }
-  }
-
   constructor(config, opts) {
     super(config, opts)
     this.opts.all = true
