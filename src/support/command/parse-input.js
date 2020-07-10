b75a00cbc37a4268cacdf0878a76a21b806916b3
Rody Kirwan <Rody.Kirwan@smartbear.com>
2020-07-10 14:06:21 +0100

    STEAM-3733 Move oas utils to dedicated directory

--- src/support/command/parse-input.js
+++ src/support/command/parse-input.js
@@ -1,7 +1,4 @@
 const { CLIError } = require('@oclif/errors')
-const { hasJsonStructure } = require('../../utils/general')
-const { safeLoad } = require('js-yaml')
-const { existsSync, readFileSync } = require('fs-extra')
 
 const versionOptionalRegex = new RegExp(/^[\w\-.]+\/[\w\-.]+(\/[\w\-.]+)?$/)
 const requiredVersionRegex = new RegExp(/^[\w\-.]+\/[\w\-.]+\/[\w\-.]+$/)
@@ -37,41 +34,9 @@ const reqType = ({ json }) => json ? 'json' : 'yaml'
 
 const resolvedParam = ({ resolved }) => resolved ? { resolved: true } : null
 
-const getOasVersion = ({ swagger, openapi }) => {
-  if (!swagger && !openapi) {
-    throw new CLIError('Cannot determine OAS version from file')
-  }
-  return swagger || openapi
-}
-
-const getVersion = definition => {
-  if (!definition.info || !definition.info.version) {
-    throw new CLIError('Cannot determine version from file')
-  }
-  return definition.info.version
-}
-
-const parseDefinition = fileName => {
-  if (!existsSync(fileName)) {
-    throw new CLIError(`File '${fileName}' not found`)
-  }
-  const file = readFileSync(fileName)
-  if (file.length === 0) {
-    throw new CLIError(`File '${fileName}' is empty`)
-  }
-  try {
-    return hasJsonStructure(file) ? JSON.parse(file) : safeLoad(file) 
-  } catch (e) {
-    throw new CLIError(`There was a problem with parsing ${fileName}. Ensure the definition is valid. ${e}`)
-  }
-}
-
 module.exports = {
   getApiIdentifierArg,
   getDomainIdentifierArg,
-  getOasVersion,
-  getVersion,
-  parseDefinition,
   reqType,
   resolvedParam,
   validateObjectIdentifier
