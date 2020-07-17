f95d94948d7e13618cbc7d2bef110f4cd4fa903f
michaelmelody91 <michael.melody@smartbear.com>
2020-07-17 15:14:22 +0100

    Removing console.log

--- src/config/index.js
+++ src/config/index.js
@@ -22,7 +22,6 @@ const setConfig = update => {
 
 const isURLValid = () => {
   const { SWAGGERHUB_URL } = getConfig()
-  console.log(SWAGGERHUB_URL.endsWith('/v1'))
   return SWAGGERHUB_URL.endsWith('api.swaggerhub.com') || (!SWAGGERHUB_URL.includes('api.swaggerhub.com') && SWAGGERHUB_URL.endsWith('/v1'))
 }
 
