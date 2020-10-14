145738d43ba9304904d2edf3dd3ac92fe5ae5e7b
ali-smartbear <alireza.kasaaian@smartbear.com>
2020-10-14 10:03:47 +0100

    public flag also added

--- src/commands/api/visibility.js
+++ src/commands/api/visibility.js
@@ -9,7 +9,7 @@ class VisibilityCommand extends BaseCommand {
         const apiPath = getApiIdentifierArg(args)
         const [owner, name] = splitPathParams(apiPath)
         const version = await this.getDefaultApiVersion([owner, name])
-        const isPrivate = !!flags.private
+        const isPrivate = !!flags.private || !flags.public
         const updateApiObj = {
           pathParams: [owner, name, version, 'settings', 'private'],
           body: JSON.stringify({ private: isPrivate })
@@ -41,6 +41,13 @@ VisibilityCommand.flags = {
         description: 'sets the default API version as private',
         default: false,
         required: false,
+        exclusive: ['public'],
+    }),
+    public: flags.boolean({
+        description: 'sets the default API version as public',
+        default: false,
+        required: false,
+        exclusive: ['private'],
     }),
     ...BaseCommand.flags
 }
